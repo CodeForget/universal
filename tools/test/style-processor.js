b7960b70964899a1886b4dc150fde711a7fb85ef
Shivam Srivastava <srivastava998@gmail.com>
2019-08-28 04:03:04 +0530

    build(webpack): first commit
    perf(release): need to depend on latest rxjs and zone.js
    
    The version in our package.json on angular 7

---
+++ tools/test/style-processor.js
@@ -0,0 +1,9 @@
+const babelJest = require('babel-jest');
+
+module.exports = {
+  process: function(src, filename) {
+    if (filename.match(/\.(css|less|scss|styl|sss)$/)) return '';
+
+    return babelJest.process(src, filename);
+  }
+};
